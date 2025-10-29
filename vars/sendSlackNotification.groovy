c0fe5fd49f227a18f65651b915f4cf2f25efcfd2
mayurthitame <mayurthitame@gmail.com>
2025-10-29 14:58:29 +0530

    updated code

--- vars/sendSlackNotification.groovy
+++ vars/sendSlackNotification.groovy
@@ -3,4 +3,5 @@ def call(String buildStatus,String channel) {
     def message = "*${buildStatus}*: Job `${env.JOB_NAME}` - Build #${env.BUILD_NUMBER} \n" +
                   "Details: ${env.BUILD_URL}console"
     slackSend(channel: channel, color: color, message: message)
+    
 }
